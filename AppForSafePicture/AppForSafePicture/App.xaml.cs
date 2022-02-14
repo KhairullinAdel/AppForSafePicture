fb15f099e0f22460d0420da0e82fbd94b0fa00ff
KhairullinAdel <82815241+KhairullinAdel@users.noreply.github.com>
2022-02-14 19:38:56 +0300

    Additions into App.xaml for working with a database

--- AppForSafePicture/AppForSafePicture/App.xaml.cs
+++ AppForSafePicture/AppForSafePicture/App.xaml.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace AppForSafePicture
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "Items.db"
         public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
-
+        public static ItemBag db;
+        public static ItemBag DB
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new ItemBag(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return db;
+            }
+        }
         protected override void OnStart()
         {
         }
