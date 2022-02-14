ff80d008d52cb2a7981c63f7c78e36d0bd21f2f7
KhairullinAdel <82815241+KhairullinAdel@users.noreply.github.com>
2022-02-14 19:30:47 +0300

    ItemBag for SQLite

--- AppForSafePicture/AppForSafePicture/ForSQLite/ItemBag.cs
+++ AppForSafePicture/AppForSafePicture/ForSQLite/ItemBag.cs
@@ -1,10 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
-namespace AppForSafePicture.ForSQLite
+namespace AppForSafePicture
 {
-    internal class ItemBag
+    public class ItemBag
     {
+        SQLiteConnection db;
+        public ItemBag(string dbPath)
+        {
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<CustomItem>();
+        }
+
+        public IEnumerable<CustomItem> GetAll()
+        {
+            return db.Table<CustomItem>().ToList();
+        }
+
+        public CustomItem GetCertain(int id)
+        {
+            return db.Get<CustomItem>(id);
+        }
+
+        public int DeleteCertain(int id)
+        {
+            return db.Delete<CustomItem>(id);
+        }
+
+        public int SaveNew(CustomItem c)
+        {
+            if (c.Id != 0)
+            {
+                db.Update(c);
+                return c.Id;
+            }
+            else
+            {
+                return db.Insert(c);
+            }
+        }
     }
 }
